af9c536c5e3248c3ba6df01b3c1fe0d0b885c608
liuemi001 <emily712@stanford.edu>
2023-10-25 22:24:19 -0700

    ica on eeg samples

--- decision_trees.py
+++ decision_trees.py
@@ -1,10 +1,16 @@
-from sklearn import tree
 import torch
 import numpy as np
+import pandas as pd
 import pprint
+
+from sklearn import tree
+from sklearn.decomposition import FastICA
+from sklearn.metrics import accuracy_score
+
+
 pp = pprint.PrettyPrinter(indent=4)
 
-base_path = "../DreamDiffusion-main/dreamdiffusion/datasets/"
+base_path = "../ThoughtToImage/DreamDiffusion-main/dreamdiffusion/datasets/"
 eeg_14_70_path = base_path + "eeg_14_70_std.pth" # Likely refers to freq range
 eeg_5_95_path = base_path + "eeg_5_95_std.pth"
 eeg_55_95_path = base_path + "eeg_55_95_std.pth"
@@ -12,14 +18,13 @@ eeg_signals_raw_with_mean_path = base_path + "eeg_signals_raw_with_mean_std.pth"
 block_splits_by_image_all_path = base_path + "block_splits_by_image_all.pth"
 block_splits_by_image_path = base_path + "block_splits_by_image_single.pth"
 
-# load all feature and label data
-eeg_14_70_data = torch.load(eeg_14_70_path)
 
-features = [data['eeg'] for data in eeg_14_70_data['dataset']]
-print(features)
-#features = np.array(features)  # TODO: cannot coerce into a numpy array currently...
+features = np.loadtxt("ica_eeg_14_70_data.csv", delimiter=",", dtype=float)
+eeg_5_95_data = torch.load(eeg_5_95_path)
+labels = np.array([eeg_5_95_data['labels']])
+features.reshape(labels.size, features.size/labels.size)
 
-labels = eeg_14_70_data['labels']
+
 
 # train validation test split
 block_splits_by_image_all_data = torch.load(block_splits_by_image_all_path)
@@ -34,15 +39,20 @@ y_val = labels[val_indices]
 x_test = features[test_indices]
 y_test = features[test_indices]
 
-
-# compress tensors into a reasonable number of features
-
 # train on a couple different depths of decision trees
-clf = tree.DecisionTreeClassifier()
+model = tree.DecisionTreeClassifier()
+model.fit(x_train, y_train)
 
 # plot results
 
 # test on validation set
+y_val_pred = model.predict(x_val)
+y_test_pred = model.predict(x_test)
+
+accuracy_val = accuracy_score(y_val.cpu().numpy(), y_val_pred)
+accuracy_test = accuracy_score(y_test.cpu().numpy(), y_test_pred)
+print("accuracy val: ", accuracy_val)
+print("accuracy_test: ", accuracy_test)
 
 # pick best tree
 
